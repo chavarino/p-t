cadbba4d544d0ed042b17695e27df7daeaed43b5
perico <javier.chavarino.1991@gmail.com>
2019-11-11 23:18:21 +0100

    subida

--- server/imports/libAux/sharedPass.ts
+++ server/imports/libAux/sharedPass.ts
@@ -1,1 +1,29 @@
 export let secretshared ="50343c0ca9e6fb888930b60feef77c03";
+
+export const SecretServices = {
+    prod : {
+        facebook : {
+            service: "facebook",
+            appId: '489516028440012',
+            secret: 'c54d90399e4e48803885d6aa9c6cadbf'
+          },
+         google : {
+            service: "google",
+            clientId: "671960522265-hii7qilu9242c9k26cu33nu0t1f9221s.apps.googleusercontent.com",
+            secret: "PbL0BR0eCXNGZX-AOdHBAw_z"
+          }
+    },
+    dev : {
+        facebook : {
+            service: "facebook",
+            appId: '489516028440012',
+            secret: 'c54d90399e4e48803885d6aa9c6cadbf'
+          },
+         google : {
+            service: "google",
+            clientId: "671960522265-hii7qilu9242c9k26cu33nu0t1f9221s.apps.googleusercontent.com",
+            secret: "PbL0BR0eCXNGZX-AOdHBAw_z"
+          }
+    }
+
+}
