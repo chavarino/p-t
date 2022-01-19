b64f8d964db4884d9b3e5e538ccc9edfe2721ac1
chavarino <32093884+chavarino@users.noreply.github.com>
2022-01-19 16:58:40 +0100

    Update sharedPass.ts

--- server/imports/libAux/sharedPass.ts
+++ server/imports/libAux/sharedPass.ts
@@ -1,49 +1,49 @@
-export let secretshared ="50343c0ca9e6fb888930b60feef77c03";
+export let secretshared ="";
 
 export const test  ={
     isTest : true,
     pm : ["pm_card_visa","pm_card_visa_debit","pm_card_mastercard"],
-    cards: ["4242424242424242"]
+    cards: [""]
 }
 export const SecretServices = {
     prod : {
         facebook : {
-            service: "facebook",
-            appId: '489516028440012',
-            secret: 'c54d90399e4e48803885d6aa9c6cadbf'
+            service: "",
+            appId: "",
+            secret: ""
           },
          google : {
-            service: "google",
-            clientId: "671960522265-hii7qilu9242c9k26cu33nu0t1f9221s.apps.googleusercontent.com",
-            secret: "PbL0BR0eCXNGZX-AOdHBAw_z"
+            service: "",
+            clientId: "",
+            secret: ""
           },
           stripe : {
 
-            secret : "sk_test_wFBgb0r4Kv2YgY5EIWEVsaYb00KkSnycJv"
+            secret : ""
           },
           tokbox : {
-            key : "46491982",
-            secret: "fa5ce60fdb349e1603b02da545e1055b4b1ff62b"
+            key : "",
+            secret: ""
           }
     },
     dev : {
         facebook : {
-            service: "facebook",
-            appId: '489516028440012',
-            secret: 'c54d90399e4e48803885d6aa9c6cadbf'
+            service: "",
+            appId: "",
+            secret: ""
           },
          google : {
-            service: "google",
-            clientId: "671960522265-hii7qilu9242c9k26cu33nu0t1f9221s.apps.googleusercontent.com",
-            secret: "PbL0BR0eCXNGZX-AOdHBAw_z"
+            service: "",
+            clientId: "",
+            secret: ""
           },
           stripe : {
 
-            secret : "sk_test_wFBgb0r4Kv2YgY5EIWEVsaYb00KkSnycJv"
+            secret : ""
           },
           tokbox : {
-            key : "46491982",
-            secret: "fa5ce60fdb349e1603b02da545e1055b4b1ff62b"
+            key : "",
+            secret: ""
           }
     }
 
